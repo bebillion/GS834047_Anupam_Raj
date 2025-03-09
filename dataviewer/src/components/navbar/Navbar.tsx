76df89568bcdde1bb1107345177ef5e5c6bba25e
Anupam Raj <anupam24052000@gmail.com>
2025-03-09 17:39:06 +0530

    Created Navbar

--- dataviewer/src/components/navbar/Navbar.tsx
+++ dataviewer/src/components/navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UserDropDown from '../userDropDown/UserDropDown'
 
-
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white-600 text-white p-4 flex justify-between items-center">
-      <img src="/Gsynergy Logo V2 Long Description.svg" alt="GSynergy Logo" className="h-10" />  
+    <nav className="bg-white text-white p-4 flex justify-between items-center shadow-md">
+      <img src="/Gsynergy Logo V2 Long Description.svg" alt="GSynergy Logo" className="h-10 w-auto" />  
+      <div className="flex-1 text-center">
+        <span className="text-xl font-bold text-black">Data Viewer App</span>
+      </div>
       <UserDropDown />
     </nav>
   );
