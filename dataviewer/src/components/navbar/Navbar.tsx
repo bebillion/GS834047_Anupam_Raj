1099adf09b00d72f6d6d109a5debc052cab83d6d
Anupam Raj <anupam24052000@gmail.com>
2025-03-10 23:52:40 +0530

    triggering deployment

--- dataviewer/src/components/navbar/Navbar.tsx
+++ dataviewer/src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
     <nav className="bg-white text-white p-4 flex justify-between items-center shadow-md">
       <img src="/Gsynergy Logo V2 Long Description.svg" alt="GSynergy Logo" className="h-10 w-auto" />  
       <div className="flex-1 text-center">
-        <span className="text-xl font-bold text-black">Data Viewer App</span>
+        <span className="text-xl font-bold text-black">Data Viewer APP</span>
       </div>
       <UserDropDown />
     </nav>
