b7a1db996a2fe67980cd78cb7c2f6197fb6d20f3
Anupam Raj <anupam24052000@gmail.com>
2025-03-19 13:03:14 +0530

    Adding logo

--- dataviewer/src/components/navbar/Navbar.tsx
+++ dataviewer/src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import UserDropDown from '../userDropDown/UserDropDown'
+import { MdPivotTableChart } from "react-icons/md";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-white text-white p-4 flex justify-between items-center shadow-md">
-      <img src="/Gsynergy Logo V2 Long Description.svg" alt="GSynergy Logo" className="h-10 w-auto" />  
+       <MdPivotTableChart className="text-3xl text-black" />  
       <div className="flex-1 text-center">
         <span className="text-xl font-bold text-black">Data Viewer APP</span>
       </div>
